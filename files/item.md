2af0f3fc4fd76ca38b8caba194ae1991497013ee
John Chan <chenfazhun@163.com>
2019-01-17 17:39:31 +0800

    Modify main

--- files/item.md
+++ files/item.md
@@ -1,5 +1,12 @@
 # item
-name | value
----|---
-test | 1234
-tttt | 卧槽
+name | type | default | remark
+---|---|---|---
+id | int | auto | ID
+cid | int | | 分类id
+name | varchar(20) | "" | 名称
+description | varchar(255) | null | 描述
+coverImg | varchar(255) | null | 封面图
+content | text | "" | 内容
+raisingMoney | double | 0.00 | 标的估值
+investmentCycle | int | 0 | 投资期限(年)
+region | string | | 项目地域
--- main.py
+++ main.py
@@ -3,6 +3,22 @@ if __name__ == '__main__':
 
     print 'Genesis start...'
     f = open("./files/item.md")
-
+    i = 0
     for line in f.readlines():
-        print line
+        i = i + 1
+        if i <= 3:
+            continue
+        print "line = ", line
+        elements = line.split("|")
+        print "elements = ", elements
+        name = elements[0].strip()
+        type = elements[1].strip()
+        #
+        default = elements[2].strip()
+        remark = elements[3].strip()
+
+        if type.lower() in ['string', 'text', 'varchar']:
+            print 'private String ' + name + ";"
+
+        if type == 'int' or type == 'Integer':
+            print 'private Integer ' + name + ";"
